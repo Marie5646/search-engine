86e74711740b71799ac0fcc39c503c8051aa533a
Marie5646 <125408068+Marie5646@users.noreply.github.com>
2023-07-02 18:42:20 -0500

    project-complete

---
+++ src/main/java/com/example/searchengine/controllers/RestApiController.java
@@ -0,0 +1,46 @@
+package com.example.searchengine.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+
+
+@CrossOrigin
+@RestController
+@RequestMapping(value = "/search",headers = "Accept=application/json")
+public class RestApiController {
+    @Value("${api.key}")
+    private String apiKey;
+
+    @GetMapping("/searchForm")
+    public String search(@RequestParam(name = "search")String search, Model model) throws IOException, InterruptedException {
+        String encodedSearch = URLEncoder.encode(search, StandardCharsets.UTF_8);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://google.serper.dev/search?q=" + encodedSearch))
+                .header("X-API-KEY",apiKey )
+                .header("Content-Type", "application/json")
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+
+        model.addAttribute("response", response.body());
+
+          return response.body();
+    }
+
+
+
+}
+
+
+
---
+++ src/main/java/com/example/searchengine/controllers/SearchController.java
@@ -0,0 +1,41 @@
+package com.example.searchengine.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+
+@Controller
+public class SearchController {
+    @GetMapping("/search")
+    public String getPage(@ModelAttribute String response) {
+        return "/search";
+    }
+
+    @GetMapping("/searchForm")
+    public String search(@RequestParam(name = "search") String search, Model model) throws IOException, InterruptedException {
+        String encodedSearch = URLEncoder.encode(search, StandardCharsets.UTF_8);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://google.serper.dev/search?q=" + encodedSearch))
+                .header("X-API-KEY", "6afd8aa8e4e9fa5b145b55cc32f7fa29a82c6e84")
+                .header("Content-Type", "application/json")
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+
+        model.addAttribute("response", response.body());
+
+        return response.body();
+    }
+
+
+}
