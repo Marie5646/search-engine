ff04d9d74ef896ba7d6b50c310ed48a60759b7eb
Marie5646 <125408068+Marie5646@users.noreply.github.com>
2023-07-04 15:07:07 -0500

    key updated

--- src/main/java/com/example/searchengine/controllers/RestApiController.java
+++ src/main/java/com/example/searchengine/controllers/RestApiController.java
@@ -1,7 +1,5 @@
 package com.example.searchengine.controllers;
-
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
@@ -11,23 +9,23 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+import java.util.function.Supplier;
 
 
 @CrossOrigin
 @RestController
 @RequestMapping(value = "/search",headers = "Accept=application/json")
-@PropertySource("classpath:application.properties")
 public class RestApiController {
-    @Value("${api.key}")
+    @Value("${API_KEY}")
     private String apiKey;
-
     @GetMapping("/searchForm")
     public String search(@RequestParam(name = "search")String search, Model model) throws IOException, InterruptedException {
+
         String encodedSearch = URLEncoder.encode(search, StandardCharsets.UTF_8);
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("https://google.serper.dev/search?q=" + encodedSearch))
-                .header("X-API-KEY",apiKey )
+                .header("X-API-KEY", apiKey )
                 .header("Content-Type", "application/json")
                 .GET()
                 .build();
--- src/main/java/config/SecurityConfiguration.java
+++ src/main/java/config/SecurityConfiguration.java
@@ -1,24 +1,24 @@
 package config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import java.util.function.Supplier;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Bean
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .requestMatchers("/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated();
-
-
+                .antMatchers("/static/**").permitAll() // Allow access to static resources
+                .anyRequest().authenticated() // Require authentication for other requests
+                .and()
+                .csrf().disable(); // Disable CSRF protection for simplicity
     }
 
 
